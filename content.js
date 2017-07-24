852ddb33b8cc3b43c3a2611c7d5d6288876a90df
katenorquay <katenorquay@live.com>
2017-07-24 17:12:31 +1200

    an embarrassing event listener

---
+++ content.js
@@ -0,0 +1,32 @@
+function getButton() {
+    var nextBtn = document.getElementsByClassName("nextBtn")
+    console.log('hello')
+    if (nextBtn.length == 0) {
+      setTimeout(function(){
+        var nextBtn = document.getElementsByClassName("nextBtn")
+        addEventListener(nextBtn)
+     }, 15000);
+    }
+
+
+
+
+function addEventListener(nextBtn) {
+  console.log('the button', nextBtn)
+  nextBtn[0].addEventListener('click', callSetTimeOut);
+}
+
+  var time = 20000
+
+  function callSetTimeOut() {
+    setTimeout(function(){
+      alert("Hello");
+   }, 3000);
+  }
+}
+
+
+window.addEventListener("load", function load(event){
+    window.removeEventListener("load", load, false); //remove listener, no longer needed
+    getButton()
+},false);
